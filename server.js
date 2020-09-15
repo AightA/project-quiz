b9b30e15a206546aae5f02ce5cf1ec75ae81e828
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-09-15 23:02:14 +0100

    stage 1 deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 const quizRouter = require('./routes/quizzes');
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -17,6 +18,16 @@ db.once('open', () => console.log('Connected to database'));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use('/quizzes', quizRouter);
+
+// use static assets from client built on heroku
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
