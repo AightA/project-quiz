5f7bc72b7e5c59f8d009de7e4f936a2ccaf426fa
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-09-23 20:27:39 +0100

    deploy fix

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use(morgan('tiny'));
 app.use(express.json());
 app.use('/quizzes', quizRouter);
 
-// use static assets from client built on heroku
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // use static assets from client built on heroku
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
